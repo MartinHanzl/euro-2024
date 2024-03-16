b0138a5f81951cb73adce495b5eb2719a995cbbf
martashanzl2000 <martin.hanzl@monstermedia.cz>
2024-03-16 09:02:16 +0100

    update auth user and layout

--- admin/nuxt.config.ts
+++ admin/nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Euro 2024'
+        }
+    },
     devtools: {enabled: true},
     modules: [
         '@nuxtjs/tailwindcss',
         '@nuxtjs/i18n',
-        '@pinia/nuxt'
+        '@pinia/nuxt',
+        '@nuxtjs/eslint-module'
     ],
     i18n: {
         defaultLocale: 'cs',
@@ -20,5 +28,8 @@ export default defineNuxtConfig({
                 file: './lang/en.json',
             }
         ]
+    },
+    eslint: {
+        cache: false
     }
 })
--- admin/store/auth.ts
+++ admin/store/auth.ts
@@ -1,7 +1,7 @@
 // store/auth.ts
 // @ts-ignore
 import { defineStore } from 'pinia';
-import {useRouter} from "#app";
+import auth from "~/middleware/auth";
 
 interface UserPayloadInterface {
     username: string;
@@ -12,6 +12,7 @@ export const useAuthStore = defineStore('auth', {
     state: () => ({
         authenticated: false,
         loading: false,
+        authUser: null
     }),
     actions: {
         async authenticateUser({ username, password }: UserPayloadInterface) {
@@ -32,23 +33,30 @@ export const useAuthStore = defineStore('auth', {
                 // @ts-ignore
                 token.value = data?.value?.access_token; // set token to cookie
                 this.authenticated = true; // set authenticated  state value to true
+
+                this.authUser = await $fetch('http://localhost:8000/api/auth/me', {
+                    method: 'post',
+                    headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer' + data?.value?.access_token },
+                });
             }
         },
         async refreshToken() {
             const { data, pending }: any = await useFetch('http://localhost:8000/api/auth/me', {
                 method: 'post',
-                headers: { 'Content-Type': 'application/json', 'Authorization': useCookie('token') }
+                headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer' + useCookie('token') }
             });
 
                 const token = useCookie('token');
             if (data.value) {
                 // @ts-ignore
                 token.value = data?.value?.access_token;
+                this.authUser = data?.value;
             }
         },
         logUserOut() {
             const token = useCookie('token'); // useCookie new hook in nuxt 3
             this.authenticated = false; // set authenticated  state value to false
+            this.authUser = null;
             // @ts-ignore
             token.value = null; // clear the token cookie
         },
